b27414768bff303779511cf39ddde95b7283d073
phyzical <5182053+phyzical@users.noreply.github.com>
2023-02-17 11:41:48 +0800

    try rnning iwthout if statment

--- additional_tasks.sh
+++ additional_tasks.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e
 
-DOCKER_BUILDKIT=1 $BUILD_PRODUCTION_COMMAND > build.txt &
-[ "$EXTRA_TASK_1" != "" ] && $EXTRA_TASK_1 > EXTRA_TASK_1.txt &
-[ "$EXTRA_TASK_2" != "" ] && $EXTRA_TASK_2 > EXTRA_TASK_2.txt &
-[ "$EXTRA_TASK_3" != "" ] && $EXTRA_TASK_3 > EXTRA_TASK_3.txt &
-[ "$EXTRA_TASK_4" != "" ] && $EXTRA_TASK_4 > EXTRA_TASK_4.txt &
-[ "$EXTRA_TASK_5" != "" ] && $EXTRA_TASK_5 > EXTRA_TASK_5.txt &
-[ "$EXTRA_TASK_6" != "" ] && $EXTRA_TASK_6 > EXTRA_TASK_6.txt &
+# DOCKER_BUILDKIT=1 $BUILD_PRODUCTION_COMMAND > build.txt &
+$EXTRA_TASK_2 > EXTRA_TASK_2.txt &
+# [ "$EXTRA_TASK_1" != "" ] && $EXTRA_TASK_1 > EXTRA_TASK_1.txt &
+# [ "$EXTRA_TASK_2" != "" ] && $EXTRA_TASK_2 > EXTRA_TASK_2.txt &
+# [ "$EXTRA_TASK_3" != "" ] && $EXTRA_TASK_3 > EXTRA_TASK_3.txt &
+# [ "$EXTRA_TASK_4" != "" ] && $EXTRA_TASK_4 > EXTRA_TASK_4.txt &
+# [ "$EXTRA_TASK_5" != "" ] && $EXTRA_TASK_5 > EXTRA_TASK_5.txt &
+# [ "$EXTRA_TASK_6" != "" ] && $EXTRA_TASK_6 > EXTRA_TASK_6.txt &
 
 wait
 
