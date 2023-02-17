7fb86f6765aa55ec4c2f6e838c25226d4af0bfbf
phyzical <5182053+phyzical@users.noreply.github.com>
2023-02-17 15:30:16 +0800

    try again

--- additional_tasks.sh
+++ additional_tasks.sh
@@ -19,7 +19,7 @@ error() {
 
 # DOCKER_BUILDKIT=1 $BUILD_PRODUCTION_COMMAND > build.txt &
 echo "$EXTRA_TASK_2"
-$EXTRA_TASK_2
+$($(echo $EXTRA_TASK_2 | sed -r "s/\"/'/g"))
 # async "$EXTRA_TASK_2" success error
 # [ "$EXTRA_TASK_1" != "" ] && $EXTRA_TASK_1 > EXTRA_TASK_1.txt &
 # [ "$EXTRA_TASK_2" != "" ] && $EXTRA_TASK_2 > EXTRA_TASK_2.txt &
