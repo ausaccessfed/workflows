8949caa79db6d0b0cc7d963825ab5e84de75fc21
phyzical <5182053+phyzical@users.noreply.github.com>
2023-02-17 12:29:11 +0800

    more quotes

--- additional_tasks.sh
+++ additional_tasks.sh
@@ -14,7 +14,7 @@ error() {
 }
 
 # DOCKER_BUILDKIT=1 $BUILD_PRODUCTION_COMMAND > build.txt &
-async $EXTRA_TASK_2 success error
+async "$EXTRA_TASK_2" success error
 # [ "$EXTRA_TASK_1" != "" ] && $EXTRA_TASK_1 > EXTRA_TASK_1.txt &
 # [ "$EXTRA_TASK_2" != "" ] && $EXTRA_TASK_2 > EXTRA_TASK_2.txt &
 # [ "$EXTRA_TASK_3" != "" ] && $EXTRA_TASK_3 > EXTRA_TASK_3.txt &
