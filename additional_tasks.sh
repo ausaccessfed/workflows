e4f3b1c3e1e243f8c0a819a2866823cf93274e10
phyzical <5182053+phyzical@users.noreply.github.com>
2023-02-17 13:32:45 +0800

    add backe cho

--- additional_tasks.sh
+++ additional_tasks.sh
@@ -16,6 +16,7 @@ error() {
 }
 
 # DOCKER_BUILDKIT=1 $BUILD_PRODUCTION_COMMAND > build.txt &
+echo "$EXTRA_TASK_2"
 async "$EXTRA_TASK_2" success error
 # [ "$EXTRA_TASK_1" != "" ] && $EXTRA_TASK_1 > EXTRA_TASK_1.txt &
 # [ "$EXTRA_TASK_2" != "" ] && $EXTRA_TASK_2 > EXTRA_TASK_2.txt &
