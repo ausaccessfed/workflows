0f004e67288ee8bc72eb833baf83dfbbddb239a3
phyzical <5182053+phyzical@users.noreply.github.com>
2023-02-20 14:07:47 +0800

    fixup outputfile

--- additional_tasks.sh
+++ additional_tasks.sh
@@ -16,13 +16,13 @@ error() {
 }
 
 export DOCKER_BUILDKIT=1
-$BUILD_PRODUCTION_COMMAND 1> jobs_1.txt 2> jobs_1_error.txt &
-[ "$EXTRA_TASK_1" != "" ] && $EXTRA_TASK_1 1> jobs_2.txt 2> jobs_2_error.txt &
-[ "$EXTRA_TASK_2" != "" ] && $EXTRA_TASK_2 1> jobs_3.txt 2> jobs_3_error.txt &
-[ "$EXTRA_TASK_3" != "" ] && $EXTRA_TASK_3 1> jobs_4.txt 2> jobs_4_error.txt &
-[ "$EXTRA_TASK_4" != "" ] && $EXTRA_TASK_4 1> jobs_5.txt 2> jobs_5_error.txt &
-[ "$EXTRA_TASK_5" != "" ] && $EXTRA_TASK_5 1> jobs_6.txt 2> jobs_6_error.txt &
-[ "$EXTRA_TASK_6" != "" ] && $EXTRA_TASK_6 1> jobs_7.txt 2> jobs_7_error.txt &
+$BUILD_PRODUCTION_COMMAND 1> job_1.txt 2> job_1_error.txt &
+[ "$EXTRA_TASK_1" != "" ] && $EXTRA_TASK_1 1> job_2.txt 2> job_2_error.txt &
+[ "$EXTRA_TASK_2" != "" ] && $EXTRA_TASK_2 1> job_3.txt 2> job_3_error.txt &
+[ "$EXTRA_TASK_3" != "" ] && $EXTRA_TASK_3 1> job_4.txt 2> job_4_error.txt &
+[ "$EXTRA_TASK_4" != "" ] && $EXTRA_TASK_4 1> job_5.txt 2> job_5_error.txt &
+[ "$EXTRA_TASK_5" != "" ] && $EXTRA_TASK_5 1> job_6.txt 2> job_6_error.txt &
+[ "$EXTRA_TASK_6" != "" ] && $EXTRA_TASK_6 1> job_7.txt 2> job_7_error.txt &
 
 wait
 
