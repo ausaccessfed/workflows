9dca100baa867be0be007c6ed088bd814f072fc1
phyzical <5182053+phyzical@users.noreply.github.com>
2023-02-20 13:59:51 +0800

    lets try a different way

--- additional_tasks.sh
+++ additional_tasks.sh
@@ -16,13 +16,13 @@ error() {
 }
 
 export DOCKER_BUILDKIT=1
-async "$BUILD_PRODUCTION_COMMAND" success error
-[ "$EXTRA_TASK_1" != "" ] && async "$EXTRA_TASK_1" success error
-[ "$EXTRA_TASK_2" != "" ] && async "$EXTRA_TASK_2" success error
-[ "$EXTRA_TASK_3" != "" ] && async "$EXTRA_TASK_3" success error
-[ "$EXTRA_TASK_4" != "" ] && async "$EXTRA_TASK_4" success error
-[ "$EXTRA_TASK_5" != "" ] && async "$EXTRA_TASK_5" success error
-[ "$EXTRA_TASK_6" != "" ] && async "$EXTRA_TASK_6" success error
+$BUILD_PRODUCTION_COMMAND 1> jobs_1.txt 2> jobs_1_error.txt &
+[ "$EXTRA_TASK_1" != "" ] && $EXTRA_TASK_1 1> jobs_2.txt 2> jobs_2_error.txt &
+[ "$EXTRA_TASK_2" != "" ] && $EXTRA_TASK_2 1> jobs_3.txt 2> jobs_3_error.txt &
+[ "$EXTRA_TASK_3" != "" ] && $EXTRA_TASK_3 1> jobs_4.txt 2> jobs_4_error.txt &
+[ "$EXTRA_TASK_4" != "" ] && $EXTRA_TASK_4 1> jobs_5.txt 2> jobs_5_error.txt &
+[ "$EXTRA_TASK_5" != "" ] && $EXTRA_TASK_5 1> jobs_6.txt 2> jobs_6_error.txt &
+[ "$EXTRA_TASK_6" != "" ] && $EXTRA_TASK_6 1> jobs_7.txt 2> jobs_7_error.txt &
 
 wait
 
