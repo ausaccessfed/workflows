639814d80ef7f06ffc9938a82cd5dbc4c003f51e
phyzical <5182053+phyzical@users.noreply.github.com>
2023-02-20 16:40:18 +0800

    fix up error code

--- additional_tasks.sh
+++ additional_tasks.sh
@@ -11,20 +11,18 @@ error() {
     local _content="$1"
     local _err="$2"
     local _ID="$3"
-    echo "result ${_content}"
     echo "${_content}" > job_${_ID}.txt
-    echo "error ${_err}"
     echo "${_err}" > job_${_ID}_error.txt
 }
 
 export DOCKER_BUILDKIT=1
-async "$BUILD_PRODUCTION_COMMAND" success failure
-[ "$EXTRA_TASK_1" != "" ] && async "$EXTRA_TASK_1" success failure
-[ "$EXTRA_TASK_2" != "" ] && async "$EXTRA_TASK_2" success failure
-[ "$EXTRA_TASK_3" != "" ] && async "$EXTRA_TASK_3" success failure
-[ "$EXTRA_TASK_4" != "" ] && async "$EXTRA_TASK_4" success failure
-[ "$EXTRA_TASK_5" != "" ] && async "$EXTRA_TASK_5" success failure
-[ "$EXTRA_TASK_6" != "" ] && async "$EXTRA_TASK_6" success failure
+async "$BUILD_PRODUCTION_COMMAND" success error
+[ "$EXTRA_TASK_1" != "" ] && async "$EXTRA_TASK_1" success error
+[ "$EXTRA_TASK_2" != "" ] && async "$EXTRA_TASK_2" success error
+[ "$EXTRA_TASK_3" != "" ] && async "$EXTRA_TASK_3" success error
+[ "$EXTRA_TASK_4" != "" ] && async "$EXTRA_TASK_4" success error
+[ "$EXTRA_TASK_5" != "" ] && async "$EXTRA_TASK_5" success error
+[ "$EXTRA_TASK_6" != "" ] && async "$EXTRA_TASK_6" success error
 
 wait
 
