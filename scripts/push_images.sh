73e289f717c8d7ea1b72d68ecce5b80181298a97
phyzical <5182053+phyzical@users.noreply.github.com>
2023-02-21 15:03:58 +0800

    needs to import functions

--- scripts/push_images.sh
+++ scripts/push_images.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+source ./.github/reusable_workflow/scripts/functions.sh
 
 async "$PRODUCTION_PUSH_COMMAND" "push_production" success error
 async "$CACHE_PUSH_COMMAND" "push_cache" success error
