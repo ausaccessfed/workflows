fada301dc56fd47894a70c53651ca1ddbdc2a2b9
Jack <5182053+phyzical@users.noreply.github.com>
2023-09-14 11:05:05 +0800

    Add renovate (#29)
    * added future proofing with the auto looping up to 10 tasks
    
    * also added durations to logs
    
    * start with a simple create the issues and prs only first
    
    feature/add-renovate-for-saml

---
+++ .github/renovate-config.js
@@ -0,0 +1,34 @@
+module.exports = {
+    username: 'aaf-terraform',
+    gitAuthor: 'aaf-terraform <fishwhack9000+terraform@gmail.com>',
+    onboarding: false,
+    requireConfig: "optional",
+    platform: 'github',
+    forkProcessing: "disabled",
+    repositories: ['ausaccessfed/saml-service'],
+    labels: ["dependencies"],
+    ignorePaths: [
+        "**/node_modules/**",
+        "**/bower_components/**",
+        "**/vendor/**",
+        "**/examples/**",
+        "**/__tests__/**",
+        "**/tests/**",
+        "**/__fixtures__/**",
+        "**/.terraform/**"
+    ],
+    extends: [
+        ":dependencyDashboard",
+        ":semanticPrefixFixDepsChoreOthers",
+        "group:monorepos",
+        "group:recommended",
+        "replacements:all",
+        "workarounds:all"
+    ],
+    ignorePresets: [":prHourlyLimit2"],
+    prHourlyLimit: 0,
+    prConcurrentLimit: 0,
+    commitMessageAction: "Upgrade",
+    commitMessageTopic: "{{packageName}}/{{depName}}",
+    commitMessageExtra: "{{currentVersion}} -> {{newVersion}}"
+};
