6110ddb916af4c779dcf48adcdb8b18b4d00deb9
phyzical <5182053+phyzical@users.noreply.github.com>
2023-10-06 12:24:50 +0800

    address CIS-DI-0005 by enforcing docker_content_trust
    feature/addressing-CIS-DI-0005

--- scripts/additional_tasks.sh
+++ scripts/additional_tasks.sh
@@ -2,6 +2,7 @@
 source ./.github/reusable_workflow/scripts/functions.sh
 
 export DOCKER_BUILDKIT=1
+export DOCKER_CONTENT_TRUST=1
 async "$BUILD_PRODUCTION_COMMAND" "build" success error
 async "$EXTRA_TASK_IMAGE_LINT_COMMAND" "image_lint" success error
 for ((i=1; i<=EXTRA_TASK_INC; i++)); do
