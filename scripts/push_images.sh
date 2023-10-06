d47415c84a35797e918605e06f2b6bb608af77b5
Jack <5182053+phyzical@users.noreply.github.com>
2023-10-06 14:26:27 +0800

    add logic to push latest as a sperate async command (#40)

--- scripts/push_images.sh
+++ scripts/push_images.sh
@@ -3,6 +3,7 @@
 source ./.github/reusable_workflow/scripts/functions.sh
 
 async "$PRODUCTION_PUSH_COMMAND" "push_production" success error
+async "$PRODUCTION_LATEST_PUSH_COMMAND" "push_production_latest" success error
 async "$CACHE_PUSH_COMMAND" "push_cache" success error
 
 wait
