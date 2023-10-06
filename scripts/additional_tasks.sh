458a4315c008a377271f83d06e1d79dc205eca35
Jack <5182053+phyzical@users.noreply.github.com>
2023-10-06 11:16:40 +0800

    Support generic containers (#31)
    * Adds logic to support a new input of override_image_tag -> when provided this overwrites the magic stable-SHA will be used for generic docker builds that dont need such a hard version lock.
    * adds logic to run the image linting during build of prod and extras tasks
    * adds logic to scan for cves on prod image. this currently is only happening in master merges.

--- scripts/additional_tasks.sh
+++ scripts/additional_tasks.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 source ./.github/reusable_workflow/scripts/functions.sh
 
-
 export DOCKER_BUILDKIT=1
 async "$BUILD_PRODUCTION_COMMAND" "build" success error
+async "$EXTRA_TASK_IMAGE_LINT_COMMAND" "image_lint" success error
 for ((i=1; i<=EXTRA_TASK_INC; i++)); do
     EXTRA_TASK="EXTRA_TASK_$i"
     [ "${!EXTRA_TASK}" != "" ] && async "${!EXTRA_TASK}" "task_$i" success error
