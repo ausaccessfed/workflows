d10060f2e5900aae0e83a20434cd1b902d09f602
Jack <5182053+phyzical@users.noreply.github.com>
2023-10-12 11:23:46 +0800

    add logic to support yum repo version control (#44)

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -38,15 +38,26 @@ module.exports = {
     regexManagers: [
         {
             "description": "Update dockerfile github releases",
-            "fileMatch": [
-                "Dockerfile"
-            ],
+            "fileMatch": ["Dockerfile"],
             // i.e
             // # renovate: datasource=github-releases depName=goodwithtech/dockle
             // ARG DOCKLE_VERSION=0.4.0
+            "datasourceTemplate": "github-releases",
             "matchStrings": [
-                "#\\s*renovate:\\s*datasource=(?<datasource>.*?)\\s*depName=(?<depName>.*?)\\s*ARG\\s.*?_VERSION\\s*=\\s*\"*(?<currentValue>.*)\"*"
+                "\\s*#\\s*renovate:\\s*datasource=github-releases\\s*depName=(?<depName>.*?)\\s*ARG\\s.*?_VERSION\\s*=\\s*\"*(?<currentValue>.*)\"*"
             ]
+        },
+        {
+            "fileMatch": ["Dockerfile"],
+            // i.e
+            // # renovate: datasource=yum repo=rocky-9-appstream-x86_64
+            // jq-1.6-13.el9 \
+            "matchStrings": [
+                "\\s*#\\s*renovate:\\s*datasource=yum\\s*repo=(?<registryUrl>[^\\s]+)\\s+(?<depName>[^\\s]+)-(?<currentValue>[^\\s-]+-[^\\s-]+)"
+            ],
+            "datasourceTemplate": "npm",
+            "versioningTemplate": "loose",
+            "registryUrlTemplate": "https://yum2npm.io/repos/{{replace '/' '/modules/' registryUrl}}/packages"
         }
     ],
     packageRules: [
