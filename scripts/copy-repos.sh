b4df1bf18bac9cb635621a9da6ffe92f56d7a226
Jack <5182053+phyzical@users.noreply.github.com>
2023-11-02 08:53:08 +0800

    Debugging permission denied issues (#49)
    * add ls and bash tow void perm denied?
    
    * add logs

--- scripts/copy-repos.sh
+++ scripts/copy-repos.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "starting"
 token=$1
 items=($(gh repo list ausaccessfed -L 100 --json url,defaultBranchRef -t "{{range .}}{{.url}}/archive/refs/heads/{{.defaultBranchRef.name}}.zip {{end}}"))
 
@@ -6,7 +7,9 @@ mkdir -p tmp/repos
 for i in ${items[@]};
 do
     repo=$(echo "$i" | cut -d'/' -f 5)
+    echo "backing up $repo"
     wget -O tmp/repos/$repo.zip --header="Authorization: token ${token}" --header="Accept:application/vnd.github.v3.raw" $i 
 done
 
 aws-vault exec shared_services -- aws s3 sync tmp/repos s3://aaf-archived-repositories
+echo "finished"
