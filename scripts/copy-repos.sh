336e0dbcef6948d49246412fd36cba5e5da01b25
Jack <5182053+phyzical@users.noreply.github.com>
2023-11-02 11:12:08 +0800

    add logic to rety dls and error if any zip[s missing at end (#51)

--- scripts/copy-repos.sh
+++ scripts/copy-repos.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 echo "starting"
-token=$1
+export GH_TOKEN="$1"
 items=($(gh repo list ausaccessfed -L 100 --json url,defaultBranchRef -t "{{range .}}{{.url}}/archive/refs/heads/{{.defaultBranchRef.name}}.zip {{end}}"))
 
-mkdir -p tmp/repos
+dir=tmp/repos
+mkdir -p $dir
 for i in ${items[@]};
 do
     repo=$(echo "$i" | cut -d'/' -f 5)
     echo "backing up $repo"
-    wget -O tmp/repos/$repo.zip --header="Authorization: token ${token}" --header="Accept:application/vnd.github.v3.raw" $i 
+    wget -O $dir/$repo.zip --retry-connrefused --header="Authorization: token ${GH_TOKEN}" --header="Accept:application/vnd.github.v3.raw" $i
 done
 
-aws-vault exec shared_services -- aws s3 sync tmp/repos s3://aaf-archived-repositories
+expected=$(wc -w <<< "${items[@]}")
+actual=$(ls -1 $dir | wc -l)
+aws s3 sync $dir s3://aaf-archived-repositories
+
+if [ "$actual" != "$expected" ]; then
+    echo "ERROR! expected to upload $expected but actually uploaded $actual"
+    exit 1;
+fi
+
 echo "finished"
