53dcab7fd4a8c22958c6fc642504783e6d71f533
phyzical <5182053+phyzical@users.noreply.github.com>
2023-11-24 15:08:13 +0800

    refactor naming

--- scripts/workflow-distributor.js
+++ scripts/workflow-distributor.js
@@ -1,9 +1,66 @@
+const getRepo = async (owner, repo) => {
+    return await github.rest.repos.get({
+        owner,
+        repo,
+    });
+}
+
+const getBranch = async (owner, repo, branch) => {
+    return await github.rest.repos.getBranch({
+        owner,
+        repo,
+        branch,
+    })
+}
+
+const createBranch = async (owner, repo, branch, sha) => {
+    return await github.rest.git.createRef({
+        owner,
+        repo,
+        ref: `refs/heads/${branch}`,
+        sha
+    })
+}
+
+const createFile = async (
+    owner,
+    repo,
+    branch,
+    path,
+    message,
+    content,
+    committer
+) => {
+    return await github.rest.repos.createOrUpdateFileContents({
+        owner,
+        repo,
+        branch,
+        path,
+        message,
+        content,
+        committer
+    })
+}
+const createPR = async (
+    owner,
+    repo,
+    head,
+    base,
+    message,
+) => {
+    return await github.rest.pulls.create({
+        owner,
+        repo,
+        head,
+        base,
+        title: message,
+        body: message
+    })
+}
+
+
 const run = async ({ github, context, fs, glob }) => {
-    let {
-        repo: {
-            owner
-        }
-    } = context
+    let { repo: { owner } } = context
     const committer = context.pusher ?? {
         name: "todo",
         email: "todo"
@@ -22,46 +79,27 @@ const run = async ({ github, context, fs, glob }) => {
 
         for (let x = 0; x < repos.length; x++) {
             const repo = repos[x].split("/").pop()
-            const {
-                data: {
-                    default_branch: base
-                }
-            } = await github.rest.repos.get({
-                owner,
-                repo,
-            });
-            const {
-                data: {
-                    commit: { sha }
-                }
-            } = await github.rest.repos.getBranch({
-                owner,
-                repo,
-                branch: base,
-            })
-            await github.rest.git.createRef({
-                owner,
-                repo,
-                ref: `refs/heads/${branch}`,
-                sha
-            })
-            await github.rest.repos.createOrUpdateFileContents({
+
+            const { data: { default_branch: base } } = await getRepo(owner, repo)
+            const { data: { commit: { sha } } } = await getBranch(owner, repo, base)
+
+            await createBranch(owner, repo, branch, sha)
+            await createFile(
                 owner,
                 repo,
                 branch,
-                path: `.github/workflows/${fileNameCleaned}`,
+                `.github/workflows/${fileNameCleaned}`,
                 message,
                 content,
                 committer
-            })
-            await github.rest.pulls.create({
+            )
+            await createPR(
                 owner,
                 repo,
-                head: branch,
+                branch,
                 base,
-                title: message,
-                body: message
-            })
+                message,
+            )
         }
     }
 }
