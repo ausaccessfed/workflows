afcd5fe43e4dc444dd7dd6a2a9315edfa5504c11
phyzical <5182053+phyzical@users.noreply.github.com>
2024-05-15 10:44:29 +0800

    go down the route of using git cloneisntead

--- scripts/copy-repos.sh
+++ scripts/copy-repos.sh
@@ -1,30 +1,35 @@
 #!/bin/bash
 echo "starting"
 export GH_TOKEN="$1"
-items=($(gh repo list ausaccessfed -L 100 --json url,defaultBranchRef -t "{{range .}}{{.url}}/archive/refs/heads/{{.defaultBranchRef.name}}.zip {{end}}"))
+items=($(gh repo list ausaccessfed -L 100 --json url,defaultBranchRef -t "{{range .}}{{.url}}/{{.defaultBranchRef.name}} {{end}}"))
 
 dir=tmp/repos
-mkdir -p $dir
+zip_dir="$dir/zips"
+mkdir -p $dir $zip_dir
 for i in "${items[@]}"; do
     repo=$(echo "$i" | cut -d'/' -f 5)
+    defaultBranchRef=$(echo "$i" | cut -d'/' -f 6)
+    repoFolder="$dir/$repo"
+    if [ -d "$repoFolder" ]; then
+        echo "Pulling $repo"
+        cd "$repoFolder" || exit
+        git fetch origin "$defaultBranchRef" && git reset --hard "$defaultBranchRef"
+        cd - || exit
+    else
+        echo "Repo not found cloning $repo"
+        git clone -b "$defaultBranchRef" "https://${GH_TOKEN}@github.com/ausaccessfed/${repo}.git" "$repoFolder"
+    fi
+    pwd
     echo "backing up $repo"
-    retries=5
-    while [ $retries -gt 0 ]; do
-        wget -O "$dir/$repo.zip" --retry-connrefused --header="Authorization: token ${GH_TOKEN}" --header="Accept:application/vnd.github.v3.raw" "$i"
-        if [ $? -eq 0 ]; then
-            break
-        else
-            echo "Failed to download $repo, retrying..."
-            retries=$((retries - 1))
-            sleep 5
-        fi
-    done
+    cd "$repoFolder" || exit
+    zip -r "../zips/$repo.zip" .
+    cd - || exit
 done
 
-find $dir -type f -size 0 -delete
+find $zip_dir -type f -size 0 -delete
 expected=$(wc -w <<<"${items[@]}")
-actual=$(ls -1 $dir | wc -l)
-aws s3 sync $dir s3://aaf-archived-repositories
+actual=$(ls -1 $zip_dir | wc -l)
+aws s3 sync $zip_dir s3://aaf-archived-repositories
 
 if [ "$actual" != "$expected" ]; then
     echo "ERROR! expected to upload $expected but actually uploaded $actual"
