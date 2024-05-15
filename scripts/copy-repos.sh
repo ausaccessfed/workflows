b569fbd23f480ee8611fdb79a317bced73ab9938
phyzical <5182053+phyzical@users.noreply.github.com>
2024-05-15 09:44:40 +0800

    add 5 retries to avoid no 404

--- scripts/copy-repos.sh
+++ scripts/copy-repos.sh
@@ -5,20 +5,29 @@ items=($(gh repo list ausaccessfed -L 100 --json url,defaultBranchRef -t "{{rang
 
 dir=tmp/repos
 mkdir -p $dir
-for i in ${items[@]};
-do
+for i in "${items[@]}"; do
     repo=$(echo "$i" | cut -d'/' -f 5)
     echo "backing up $repo"
-    wget -O $dir/$repo.zip --retry-connrefused --header="Authorization: token ${GH_TOKEN}" --header="Accept:application/vnd.github.v3.raw" $i
+    retries=5
+    while [ $retries -gt 0 ]; do
+        wget -O "$dir/$repo.zip" --retry-connrefused --header="Authorization: token ${GH_TOKEN}" --header="Accept:application/vnd.github.v3.raw" "$i"
+        if [ $? -eq 0 ]; then
+            break
+        else
+            echo "Failed to download $repo, retrying..."
+            retries=$((retries - 1))
+            sleep 5
+        fi
+    done
 done
 
-expected=$(wc -w <<< "${items[@]}")
+expected=$(wc -w <<<"${items[@]}")
 actual=$(ls -1 $dir | wc -l)
 aws s3 sync $dir s3://aaf-archived-repositories
 
 if [ "$actual" != "$expected" ]; then
     echo "ERROR! expected to upload $expected but actually uploaded $actual"
-    exit 1;
+    exit 1
 fi
 
 echo "finished"
