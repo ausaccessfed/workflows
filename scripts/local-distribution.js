418fb8bc78f32df273962b460e8a1b881ede6e33
phyzical <5182053+phyzical@users.noreply.github.com>
2024-07-12 14:30:02 +0800

    trying again

--- scripts/local-distribution.js
+++ scripts/local-distribution.js
@@ -1,11 +1,14 @@
 /* eslint-disable no-undef */
-import fs from 'fs'
-import { Octokit } from '@octokit/rest'
-import { run } from './workflow-distributor.js'
+; (async () => {
+  const fs = require('fs')
+  // Use dynamic import() for ES Modules
+  const { Octokit } = await import('@octokit/rest')
+  const run = require('./workflow-distributor.js')
 
-await run({
-  github: new Octokit({ auth: process.env.GITHUB_AUTH }),
-  context: { payload: { organization: { login: 'ausaccessfed' } } },
-  repositories: JSON.parse(process.env.REPOS),
-  fs
-})
+  await run({
+    github: new Octokit({ auth: process.env.GITHUB_AUTH }),
+    context: { payload: { organization: { login: 'ausaccessfed' } } },
+    repositories: JSON.parse(process.env.REPOS),
+    fs
+  })
+})()
