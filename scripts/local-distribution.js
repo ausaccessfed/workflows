d1c3aa179915f3d1acf9abda50032ba08421ffcc
phyzical <5182053+phyzical@users.noreply.github.com>
2024-08-01 11:40:24 +0800

    Fix logic around make empty prs

--- scripts/local-distribution.js
+++ scripts/local-distribution.js
@@ -1,6 +1,7 @@
-/* eslint-disable no-undef */
+
 ; (async () => {
   const fs = require('fs')
+  const diff = require('diff')
   // Use dynamic import() for ES Modules
   const { Octokit } = await import('@octokit/rest')
   const run = require('./workflow-distributor.js')
@@ -9,6 +10,7 @@
     github: new Octokit({ auth: process.env.GITHUB_AUTH }),
     context: { payload: { organization: { login: 'ausaccessfed' } } },
     repositories: JSON.parse(process.env.REPOS),
+    diff,
     fs
   })
 })()
