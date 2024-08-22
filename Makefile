8460c8a2247b353f57f7823b1fb979103893860c
phyzical <5182053+phyzical@users.noreply.github.com>
2024-08-22 09:39:11 +0800

    making the sciprt break more so we know there are issues,

--- Makefile
+++ Makefile
@@ -2,3 +2,6 @@
 
 run-distribution:
 	@export GITHUB_AUTH=${GITHUB_AUTH} && export REPOS=${REPOS} && node ./scripts/local-distribution.js
+
+run-backup:
+	aws-vault exec shared-services -- ./scripts/copy-repos.sh ${GITHUB_AUTH}
--- scripts/copy-repos.sh
+++ scripts/copy-repos.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+set -e
+
 echo "starting"
 export GH_TOKEN="$1"
 items=($(gh repo list ausaccessfed -L 100 --json url,defaultBranchRef -t "{{range .}}{{.url}}/{{.defaultBranchRef.name}} {{end}}"))
@@ -6,29 +9,32 @@ items=($(gh repo list ausaccessfed -L 100 --json url,defaultBranchRef -t "{{rang
 dir=tmp/repos
 zip_dir="$dir/zips"
 mkdir -p $dir $zip_dir
+
 for i in "${items[@]}"; do
     repo=$(echo "$i" | cut -d'/' -f 5)
     defaultBranchRef=$(echo "$i" | cut -d'/' -f 6)
     repoFolder="$dir/$repo"
     if [ -d "$repoFolder" ]; then
         echo "Pulling $repo"
-        cd "$repoFolder" || exit
+        cd "$repoFolder"
         git fetch origin "$defaultBranchRef" && git reset --hard "$defaultBranchRef"
-        cd - || exit
+        cd -
     else
         echo "Repo not found cloning $repo"
         git clone -b "$defaultBranchRef" "https://${GH_TOKEN}@github.com/ausaccessfed/${repo}.git" "$repoFolder"
     fi
     echo "backing up $repo"
-    cd "$repoFolder" || exit
+    cd "$repoFolder"
     zip -qr "../zips/$repo.zip" .
-    cd - || exit
+    cd -
 done
 
 find $zip_dir -type f -size 0 -delete
 expected=$(wc -w <<<"${items[@]}")
 actual=$(ls -1 $zip_dir | wc -l)
 aws s3 sync $zip_dir s3://aaf-archived-repositories
+
+rm -rf $zip_dir
 
 if [ "$actual" != "$expected" ]; then
     echo "ERROR! expected to upload $expected but actually uploaded $actual"
